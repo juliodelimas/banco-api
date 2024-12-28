4582528789b5914dad9fd9703fbff037ff6bd383
Julio de Lima <juliodelimas@Julios-MBP-2.lan>
2024-12-28 02:57:10 -0500

    Implementando os testes de cobertura de decisão

--- tests/unit/transferencias/transferenciasService.test.js
+++ tests/unit/transferencias/transferenciasService.test.js
@@ -15,12 +15,22 @@ describe('Testando o método realizarTransferencia', () => {
         inserirTransferenciaStub = sinon.stub(transferenciasModel, 'inserirTransferencia').resolves();
     });
 
-    it('deve lançar erro quando a conta de origem não for encontrada', async () => {
+    it('deve lançar erro quando o valor é menor que R$ 10', async () => {
+        try {
+            await transferenciasService.realizarTransferencia(1, 2, 9.99, null);
+            expect.fail('Deveria ter falhado, pois o valor é abaixo de R$ 10');
+        } catch (err) {
+            expect(err.status).to.equal(422);
+            expect(err.message).to.equal('O valor da transferência deve ser maior ou igual a R$10,00.');
+        }
+    });
+
+    it('deve lançar erro quando o valor for maior ou igual a R$ 10 mas a conta de origem não for encontrada', async () => {
         getContaByIdStub.onCall(0).resolves(null);
         getContaByIdStub.onCall(1).resolves({ ativa: true, saldo: 1000 });
 
         try {
-            await transferenciasService.realizarTransferencia(1, 2, 100, null);
+            await transferenciasService.realizarTransferencia(1, 2, 10, null);
             expect.fail('Deveria ter falhado, pois a conta origem retornou null');
         } catch (err) {
             expect(err.status).to.equal(404);
